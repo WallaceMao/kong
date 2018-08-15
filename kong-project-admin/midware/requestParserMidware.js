4a666103c5482e16d8780d7c62052ab99b932580
Wallace Mao <maowenqiang0752@163.com>
2018-08-15 22:01:21 +0800

    remove session and enable trust proxy

--- kong-project-admin/midware/requestParserMidware.js
+++ kong-project-admin/midware/requestParserMidware.js
@@ -1,9 +1,5 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
-const expressSession = require('express-session')
-const config = require('config')
-
-const secret = config.auth.session.secret
 
 const init = app => {
   // json格式解析
@@ -12,8 +8,6 @@ const init = app => {
   app.use(express.urlencoded({ extended: false }))
   // cookie解析
   app.use(cookieParser())
-  // 处理session
-  app.use(expressSession({ secret: secret, resave: true, saveUninitialized: true }))
 }
 
 module.exports.init = init
